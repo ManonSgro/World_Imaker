e504a1a2e0afb9d1b37941ec73363aa54e2be4c6
MANSION AMELIA <amansion@localhost.localdomain>
2020-01-06 13:21:34 +0100

    3D main

--- build/doc/html/search/functions_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['radialbasisfunction',['radialBasisFunction',['../classglimac_1_1CubeList.html#a538bbaae516f90563e233ef4b183c772',1,'glimac::CubeList']]],
-  ['read',['read',['../classglimac_1_1CubeList.html#a59d87cac2e218d617940ccdab5235b31',1,'glimac::CubeList']]]
-];
--- build/doc/html/search/functions_e.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['texture',['Texture',['../classglimac_1_1Texture.html#ac5ae4d92bf698dd77fb8135ae2329102',1,'glimac::Texture']]],
-  ['translatevertices',['translateVertices',['../classglimac_1_1Cube.html#a57a197e614422f798de48546c3a31b86',1,'glimac::Cube']]]
-];
